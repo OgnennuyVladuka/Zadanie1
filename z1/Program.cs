81ccf5c6de74626f5146399020baa01019252e73
OgnennuyVladuka <146809006+OgnennuyVladuka@users.noreply.github.com>
2023-10-08 22:21:28 +0300

    Add files via upload

---
+++ z1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace z1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите четвёртое число:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            int res = (a + b + c + d) / 4;
+            Console.WriteLine("Среднее арифметическое для чисел " + (a) + ", " + (b) + ", " + (c) + ", " + (d) + " = " + (res));
+        }
+    }
+}
